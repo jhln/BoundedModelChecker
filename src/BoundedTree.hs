1c18883a3fb117122b5aa12c60bebb0b16bbeafd
jhln <eingang@etagefuenf.com>
2021-10-05 11:56:56 +0200

    stopped by decision to add a name field for the tree data constructor NewVar and Tree suggestion

--- src/BoundedTree.hs
+++ src/BoundedTree.hs
@@ -4,6 +4,17 @@ import qualified LogicAssambler as LA
 import BoundedSolver
 import Tree
 
+
+-- für pc=0
+testTree1 :: Tree BMSolver ()
+testTree1 = NewVar $ \v -> 
+  Add 
+    (Same 
+      (Var v) 
+      (Const 1))
+    $ Return ()
+
+
 {-
 propsotion logic encoding:
 pc=0 => i'=0, r'=0, n'=n, pc'=1
@@ -16,6 +27,38 @@ pc=5 => pc'=5
 -}
 
 
--- für pc=0
-testTree1 :: Tree BMSolver ()
-testTree1 = NewVar $ \v -> (Add (Same (Var v) (Const 1))) $ Return ()
+testTree2 :: Tree BMSolver ()
+testTree2 = NewVar $ \pc -> NewVar $ \i -> NewVar $ \r -> NewVar $ \n ->
+            NewVar $ \pc' -> NewVar $ \i' -> NewVar $ \r' -> NewVar $ \n' ->
+  Add 
+    Same (Var pc) (Const 0) 
+    $ Add 
+        Same (Var i) (Const 0) 
+        $ Add 
+            Same (Var r) (Const 0) 
+            $ Add 
+                Same (Var n) (Const 0) 
+                $ Add 
+                    Same (Var pc') (Const 0) 
+                    $ Add 
+                        Same (Var i') (Const 0) 
+                        $ Add 
+                            Same (Var r') (Const 0) 
+                            $ Add 
+                                Same (Var n') (Const 0) 
+$ (Try 
+    Add 
+      (Same (Var pc) (Const 0)) 
+      $ Add 
+        $ Same (Var i) BMSolverExpr (Tree solver a)
+    (Try 
+      (Tree solver a)
+      (Try 
+        (Tree solver a)
+        (Try 
+          (Tree solver a)
+          (Try 
+            (Tree solver a)
+            (Try 
+              (Tree solver a)
+              (Tree solver a)))))))
